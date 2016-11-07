b5e20652c8f01851ae180096a4e0d16d6c7636cc
Yury Romanov <support@purptree.com>
2016-11-07 23:47:49 +0300

    Add some features. Untested!

--- react-install.sh
+++ react-install.sh
@@ -3,21 +3,31 @@
 #!!! CURRENT VERSION IS INCOMPLETE AND NEVER BEEN TESTED !!!
 
 #Run this script inside future ReactJS project directory
-#TEMPLATESDIR=//.. INSERT TEMPLATES DIRECTORY PATH HERE ..//
+TEMPLATESDIR=./templates
 
-npm init && \
-npm install --save-dev react react-dom && \
-npm install --save-dev babel-core babel-loader babel-preset-react && \
-npm install --save-dev webpack webpack-dev-server html-webpack-plugin && \
-touch .babelrc && \
+npm init
+npm install --save react react-dom
+npm install --save-dev babel-core babel-loader babel-preset-react
+npm install --save-dev webpack webpack-dev-server html-webpack-plugin 
+touch .babelrc
+echo "{ presets: ['react'] }" > .babelrc
 
-cp $TEMPLATESDIR/babelrc.tmp ./.babelrc && \
-cp $TEMPLATESDIR/wepback.config.js.tmp ./webpack.config.js && \
+cp $TEMPLATESDIR/wepback.config.js.tmp ./webpack.config.js 
 
-mkdir ./app && \
-touch ./app/index.html && \
-touch ./app/index.js && \
+node -e 'var conf = require("./package.json"); conf.scripts={build:"webpack", start:"webpack-dev-server"}; console.log(JSON.stringify(conf, null, " "));' > package.json
 
-mkdir ./app/components && \
-touch ./app/components/App.js && \
+mkdir ./app 
+touch ./app/index.html 
+printf '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<title>Local React App</title>\n</head>\n<body>\n\t<div id="app"></div>\n</body>\n</html>' > ./app/index.html
+
+touch ./app/index.js 
+printf "var React = require('react');\nvar ReactDom = require('react-dom');\nvar App = require('./components/App');\n\nReactDom.render(\n\t<App />,\ndocument.getElementById('app')\n);" > ./add/index.js
+
+mkdir ./app/components
+touch ./app/components/App.js
+
+
+npm run build
+npm run start
+
 echo "INSTALLATION COMPLETE!"
